ed9bc9be2cac762f640bbc66e8ea6b77305f78a0
lucapitzoi <luca.pitzoi@sindria.org>
2021-06-06 16:28:47 +0200

    Added build_all.sh

---
+++ build_all.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [[ -z "$1" ]]; then
+    echo "Provide image name as first argument (eg. sindriainc/<repo-slug>)"
+    exit 1
+fi
+
+if [[ -z "$2" ]]; then
+    echo "Provide a tag version as second argument (eg. 1.0.0, local)"
+    exit 1
+fi
+
+IMAGE_NAME=$1
+TAG_VERSION=$2
+
+for PHP_VERSION in 7.0 7.1 7.2 7.4 8
+do
+	bash build.sh ${IMAGE_NAME} ${TAG_VERSION} ${PHP_VERSION}
+  docker push ${IMAGE_NAME}:${TAG_VERSION}-${PHP_VERSION}
+done
