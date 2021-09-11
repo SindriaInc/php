88fe6ac67ef200033626b44c84db456e5199b3d0
lucapitzoi <luca.pitzoi@sindria.org>
2021-09-11 19:29:11 +0200

    Added build utils

---
+++ build_php7.3.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+bash build.sh sindriainc/php 2.0.0 7.3
+docker push sindriainc/php:2.0.0-7.3
---
+++ build_php8.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+bash build.sh sindriainc/php 2.0.0 8
+docker push sindriainc/php:2.0.0-8
